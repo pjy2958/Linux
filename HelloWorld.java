48deaa4c004b2d9f7bcd2066f13a2a831b45c052
pjy2958 <pjy2958@naver.com>
2022-03-31 14:47:08 +0900

    test

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("helloworld");
+		System.out.println("hello");
+		for(int i = 0; i < 30; i++){
+			System.out.println("Hello");
+
+
+		}
+	}
+}
